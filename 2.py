94a1553e28a662d831a0d8f224a2e167bdedb204
Michelle Hu <mhu16035@usc.edu>
2024-11-21 21:20:15 -0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyN8LirEyP0mu179TMsM3Aiv",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
