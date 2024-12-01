f87ca0fa5831dc8ade1caa331d76015227f5a4d9
Michelle Hu <mhu16035@usc.edu>
2024-12-01 00:02:52 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
-# 加载数据
 # 加载数据
 @st.cache
 def load_data():
